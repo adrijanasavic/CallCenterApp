5807564c0ddf44843ad12f03b08186d25d38b143
adrijanasavic <adrijana.jovicic@gmail.com>
2022-10-07 13:59:53 +0200

    logout controller

--- router/admin.js
+++ router/admin.js
@@ -2,4 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", require("../controlers/admin/adminController"));
+
+router.get("/create/user", (req, res) => {
+    res.render("admin/adminCreateForm.ejs")
+});
 module.exports = router;
--- router/index.js
+++ router/index.js
@@ -7,4 +7,5 @@ router.use("/login", require("../controlers/loginControler"));
 
 router.use("/admin", require("./admin"));
 
+router.use("/logout", require("./logout"));
 module.exports = router;
